e098c871cad5f1a869f0c3135ff89e0fbea014cd
jiu keem <jeewoo0521@naver.com>
2020-10-22 23:45:56 +0900

    Update binary_tree_이진트리.py

--- 00_자료구조 구현/binary_tree_이진트리.py
+++ 00_자료구조 구현/binary_tree_이진트리.py
@@ -4,8 +4,8 @@ class Node:
         self.right_child = None
         self.left_child = None
 
-def traverse_inorder(node):
-    if node is not None:
-        traverse_inorder(node.left_child)
-        print(node.data)
-        traverse_inorder(node.right_child)
+    def traverse_inorder(node):
+        if node is not None:
+            traverse_inorder(node.left_child)
+            print(node.data)
+            traverse_inorder(node.right_child)
