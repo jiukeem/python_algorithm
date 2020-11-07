1160c236b8aa59bfcedf179a891dfdc5a8697f59
jiu keem <jeewoo0521@naver.com>
2020-11-07 19:30:07 +0900

    Update lev2_큰수만들기.py

--- 04_프로그래머스/lev2_큰수만들기.py
+++ 04_프로그래머스/lev2_큰수만들기.py
@@ -14,6 +14,7 @@ def solution(number, k):
 
     return number
 # Status: Timeout
+# Algorithm: Greedy
 # Note: 가장 큰 문제는 i를 0으로 돌려서 다시 처음부터 체크하게 하는 부분이다. number 가 최대 1000000자리이기 때문에 타임아웃 당하는 테스트케이스가 생긴다
 #       그렇다고 -= 1 로 하면 number = number[] + number[] 부분과 상충해서 문제가 생긴다. 다른 방법을 강구해봐야 할 듯!
 
